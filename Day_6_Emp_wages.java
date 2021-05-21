57991cd86dc8f914e0d487d58960b0c3c462c886
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-21 16:56:06 +0530

    [Ranjith] modified Day_6_Emp_wages.java

--- Day_6_Emp_wages.java
+++ Day_6_Emp_wages.java
@@ -1,2 +1,13 @@
 import java.util.*;
-
+class Day_6_Emp_wages{
+	public static void main(String args[]){
+		int isFull_time=1;
+		double check=Math.floor(Math.random()*10)%2;
+		if(isFull_time==check){
+			System.out.println("Employee is present");
+		}
+		else{
+			System.out.println("Employee is not present");
+		}
+	}
+}
